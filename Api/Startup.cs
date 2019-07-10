fe1889aad57d8587a93a49664325d03e2734f9b1
Aurimas Šimkus <saurimas@hotmail.com>
2019-07-10 13:25:46 +0300

    Fix loggin in redirecturi to be HTTPS

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -22,6 +22,13 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Fix redirecturi to be HTTPS when logging in
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
+            });
+
+
             services.AddAuthentication(options => {
                 options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
