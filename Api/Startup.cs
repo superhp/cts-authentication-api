523779bd8f2e257e56e3c2aed47f522ee96daadd
Aurimas Šimkus <saurimas@hotmail.com>
2019-07-14 20:55:38 +0300

    Test

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -76,6 +76,7 @@ namespace Api
             }
             else
             {
+                app.UseDeveloperExceptionPage();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
 
